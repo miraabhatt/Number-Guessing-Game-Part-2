f82296f74f91e5bb9ea3e8b8a74352d4805f1eb3
miraabhatt <45723490+miraabhatt@users.noreply.github.com>
2018-12-15 14:05:48 -0800

    added new length

--- NumberGuessingGamePartDos/NumberGuessingGamePartDos/ViewController.swift
+++ NumberGuessingGamePartDos/NumberGuessingGamePartDos/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var messagesTextView: UITextView!
     @IBOutlet weak var guessTextField: UITextField!
     @IBOutlet weak var messageLabel: UILabel!
+    
+    // Constants
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -34,7 +38,7 @@ extension ViewController:UITextFieldDelegate {
         // use a guard statement to unwrap textField.text and on else return true, this means that if the textfield is empty, then it will return true for should the new characters be added.
         guard let text = textField.text else { return true }
         // declare a constant called newLength that gets the text.count + the string.count - the range.length, this means that new length is equal to the existing text plus the new text minus the selected text.
-        
+        let newLength = text.count + string.count - range.length 
         
         return true
     }
